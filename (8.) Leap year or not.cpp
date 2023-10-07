98aee37f1eccf8f39956e83d000233c78ef4ff6a
Gourav Prajapat <130271599+GladiatorGod@users.noreply.github.com>
2023-10-07 18:59:46 +0530

    updated

--- (8.) Leap year or not.cpp
+++ (8.) Leap year or not.cpp
@@ -17,3 +17,24 @@ int main(){
 
     return 0;
 }
+
+// second if else method
+
+#include<iostream>
+using namespace std;
+int main() {
+    int year;
+    cout<<"Enter the year: ";
+    cin >> year;
+
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0 ))
+    {
+        cout<<"The given year is a leap year."<<endl;
+    }
+    else 
+    {
+        cout<<" The given year isn't a leap year.";
+    }
+
+    return 0;
+}
